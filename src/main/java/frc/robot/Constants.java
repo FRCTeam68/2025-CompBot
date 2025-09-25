51c2211b6f5b65b13097f08a0ea0834189813b52
jashow55 <jason.howerton@gmail.com>
2025-09-25 19:51:21 -0400

    turn on tuning -tweak elevator PID to 4, 0, 0, .1, 0, 0, 0.2

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -41,7 +41,7 @@ public final class Constants {
   public static final Mode simMode = Mode.SIM;
   public static final double loopPeriodSecs = 0.02;
   public static final boolean bypassReefDetection = true;
-  public static final boolean tuningMode = false;
+  public static final boolean tuningMode = true;
   public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
 
   public static enum Mode {
